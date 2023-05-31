f4043440a2f21437cee1521706730b176d8fb511
Kacper <kacperrymkiewicz@gmail.com>
2023-05-31 21:43:21 +0200

    Add DataContext

---
+++ StudentClinic-WebApi/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentClinic_WebApi.Models;
+
+namespace StudentClinic_WebApi.Data
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+
+    }
+}
