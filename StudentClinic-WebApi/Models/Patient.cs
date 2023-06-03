04c17504a7e9eebc0731d7b1ad778ae7b50c8a0d
Kacper <kacperrymkiewicz@gmail.com>
2023-06-03 21:13:32 +0200

    Update Patient model

--- StudentClinic-WebApi/Models/Patient.cs
+++ StudentClinic-WebApi/Models/Patient.cs
@@ -3,8 +3,10 @@
     public class Patient
     {
         public int Id { get; set; }
-        public User User { get; set; }
-        public string Allergies { get; set; }
-
+        public string? Pesel { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Allergies { get; set; }
+        public User? User { get; set; }
+        public int UserId { get; set; }
     }
 }
