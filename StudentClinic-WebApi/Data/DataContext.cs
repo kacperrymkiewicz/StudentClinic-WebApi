6fb14af7184a57378c4bd9732cc0c5c445a4c5a2
Kacper <kacperrymkiewicz@gmail.com>
2023-06-05 15:18:57 +0200

    Update visit hours

--- StudentClinic-WebApi/Data/DataContext.cs
+++ StudentClinic-WebApi/Data/DataContext.cs
@@ -21,20 +21,22 @@ namespace StudentClinic_WebApi.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<VisitSlot>().HasData(
-                new VisitSlot { Id = 1, StartTime = new TimeOnly(08, 00), EndTime = new TimeOnly(08, 30)},
-                new VisitSlot { Id = 2, StartTime = new TimeOnly(08, 30), EndTime = new TimeOnly(09, 00)},
-                new VisitSlot { Id = 3, StartTime = new TimeOnly(09, 00), EndTime = new TimeOnly(09, 30)},
-                new VisitSlot { Id = 4, StartTime = new TimeOnly(09, 30), EndTime = new TimeOnly(10, 00)},
-                new VisitSlot { Id = 5, StartTime = new TimeOnly(10, 00), EndTime = new TimeOnly(10, 30)},
-                new VisitSlot { Id = 6, StartTime = new TimeOnly(10, 30), EndTime = new TimeOnly(11, 00)},
-                new VisitSlot { Id = 7, StartTime = new TimeOnly(11, 00), EndTime = new TimeOnly(11, 30)},
-                new VisitSlot { Id = 8, StartTime = new TimeOnly(11, 30), EndTime = new TimeOnly(12, 00)},
-                new VisitSlot { Id = 9, StartTime = new TimeOnly(12, 00), EndTime = new TimeOnly(12, 30)},
-                new VisitSlot { Id = 10, StartTime = new TimeOnly(12, 30), EndTime = new TimeOnly(13, 00)},
-                new VisitSlot { Id = 11, StartTime = new TimeOnly(13, 00), EndTime = new TimeOnly(13, 30)},
-                new VisitSlot { Id = 12, StartTime = new TimeOnly(13, 30), EndTime = new TimeOnly(14, 00)},
-                new VisitSlot { Id = 13, StartTime = new TimeOnly(14, 00), EndTime = new TimeOnly(14, 30)},
-                new VisitSlot { Id = 14, StartTime = new TimeOnly(14, 30), EndTime = new TimeOnly(15, 00)}
+                new VisitSlot { Id = 1, StartTime = new TimeOnly(09, 00), EndTime = new TimeOnly(09, 30)},
+                new VisitSlot { Id = 2, StartTime = new TimeOnly(09, 30), EndTime = new TimeOnly(10, 00)},
+                new VisitSlot { Id = 3, StartTime = new TimeOnly(10, 00), EndTime = new TimeOnly(10, 30)},
+                new VisitSlot { Id = 4, StartTime = new TimeOnly(10, 30), EndTime = new TimeOnly(11, 00)},
+                new VisitSlot { Id = 5, StartTime = new TimeOnly(11, 00), EndTime = new TimeOnly(11, 30)},
+                new VisitSlot { Id = 6, StartTime = new TimeOnly(11, 30), EndTime = new TimeOnly(12, 00)},
+                new VisitSlot { Id = 7, StartTime = new TimeOnly(12, 00), EndTime = new TimeOnly(12, 30)},
+                new VisitSlot { Id = 8, StartTime = new TimeOnly(12, 30), EndTime = new TimeOnly(13, 00)},
+                new VisitSlot { Id = 9, StartTime = new TimeOnly(13, 00), EndTime = new TimeOnly(13, 30)},
+                new VisitSlot { Id = 10, StartTime = new TimeOnly(13, 30), EndTime = new TimeOnly(14, 00)},
+                new VisitSlot { Id = 11, StartTime = new TimeOnly(14, 00), EndTime = new TimeOnly(14, 30)},
+                new VisitSlot { Id = 12, StartTime = new TimeOnly(14, 30), EndTime = new TimeOnly(15, 00)},
+                new VisitSlot { Id = 13, StartTime = new TimeOnly(15, 00), EndTime = new TimeOnly(15, 30)},
+                new VisitSlot { Id = 14, StartTime = new TimeOnly(15, 30), EndTime = new TimeOnly(16, 00)},
+                new VisitSlot { Id = 15, StartTime = new TimeOnly(16, 00), EndTime = new TimeOnly(16, 30)},
+                new VisitSlot { Id = 15, StartTime = new TimeOnly(16, 30), EndTime = new TimeOnly(17, 00)}
             );
 
         }
