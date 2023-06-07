f8e8708a981e239d2305ca558d82ad475629ec3b
Kacper <kacperrymkiewicz@gmail.com>
2023-06-07 14:40:26 +0200

    Minor changes in models

--- StudentClinic-WebApi/Dtos/UserAuthorization/UserLoginDto.cs
+++ StudentClinic-WebApi/Dtos/UserAuthorization/UserLoginDto.cs
@@ -7,7 +7,7 @@ namespace StudentClinic_WebApi.Dtos.User
 {
     public class UserLoginDto
     {
-        public string EmailAddress { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
     }
 }
--- StudentClinic-WebApi/Dtos/UserAuthorization/UserRegisterDto.cs
+++ StudentClinic-WebApi/Dtos/UserAuthorization/UserRegisterDto.cs
@@ -9,8 +9,8 @@ namespace StudentClinic_WebApi.Dtos.User
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string EmailAddress { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
         public string Pesel { get; set; }
         public string PhoneNumber { get; set; }
         public string City { get; set; }
