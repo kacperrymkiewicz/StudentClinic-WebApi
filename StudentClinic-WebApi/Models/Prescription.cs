3b36271350642e2644ddd2dec5f30c2bbb3cbee3
Kacper <kacperrymkiewicz@gmail.com>
2023-06-07 14:13:20 +0200

    Add prescription model

---
+++ StudentClinic-WebApi/Models/Prescription.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentClinic_WebApi.Models
+{
+    public class Prescription
+    {
+        public int Id { get; set; }
+        public Patient Patient { get; set; }
+        public int PatientId { get; set; }
+        public Doctor Doctor { get; set; }
+        public int DoctorId { get; set; } 
+        public string Drug { get; set; }
+        public string Dosage { get; set; }
+        public string PrescriptionCode { get; set; }
+        public string Recommendations { get; set; }
+        public DateOnly Date { get; set; }
+    }
+}
